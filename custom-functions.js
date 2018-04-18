a4fc092872116d40f92385f4d107fe336d394f41
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-04-18 21:42:40 +0600

    get form data as json object

---
+++ custom-functions.js
@@ -0,0 +1,12 @@
+
+
+// Get form data as json object
+var loginFormData = loginForm.serializeFormObject();
+$.fn.serializeFormObject = function () {
+    var formData = $(this).serializeArray();
+    var result = {};
+    $.each(formData, function (index, value) {
+        result[value.name] = value.value;
+    });
+    return result;
+};
