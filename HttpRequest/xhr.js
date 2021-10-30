d25f443b5f8640bcbf4a5a57b7975de6c45ca320
Iman <imanali.cse@gmail.com>
2021-10-30 08:45:32 +0600

    xmlHttpRequest

---
+++ HttpRequest/xhr.js
@@ -0,0 +1,47 @@
+const getBtn = document.getElementById('get-btn');
+const postBtn = document.getElementById('post-btn');
+
+const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onload = (response) => {
+            if (xhr.status >= 400) {
+               reject(xhr.response);
+            } else {
+               resolve(xhr.response);
+            }
+        }
+
+        xhr.onerror = () => {
+            reject('Something went wrong');
+        }
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+}
+
+const getData = () => {
+    sendHttpRequest('GET', 'https://reqres.in/api/users')
+    .then(responseData => {
+        console.log(responseData);
+    })
+};
+
+const postData = () => {
+    sendHttpRequest('POST', 'https://reqres.in/api/register', {
+        "email": "eve.holt@reqres.in",
+        //"password": "pistol"
+    })
+    .then(responseData => {
+        console.log(responseData);
+    }).catch(err => {
+        console.log(err);
+    })
+};
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', postData);
