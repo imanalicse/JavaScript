2d8f03dae72807c65e6f15442e98a11d7de0e012
Iman <imanali.cse@gmail.com>
2021-11-30 12:19:10 +0600

    pie chart

--- Charts/ChartJS/pie.php
+++ Charts/ChartJS/pie.php
@@ -2,94 +2,84 @@
 <html>
 <head>
 <title>Page Title</title>
-<!--<script-->
-<!--  src="https://code.jquery.com/jquery-2.2.4.js"-->
-<!--  integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="-->
-<!--  crossorigin="anonymous"></script>-->
     <!--<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>-->
 <!--    <script src="assets/chart.js"></script>-->
     <script src="assets/chart.min.old.js"></script>
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>-->
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-piechart-outlabels@0.1.4/dist/chartjs-plugin-piechart-outlabels.js"></script>
-<!--    <link rel="stylesheet" href="assets/chart.css">-->
+    <link rel="stylesheet" href="assets/chart.css">
 </head>
 <body>
-<style>
-    body { background: #111; }
-    .doughnut-canvas-container { background: #FFF; }
-</style>
-<div class="container2">
-<!--    <div class="chart_container">-->
-<!--        <div>Line Chart</div>-->
-<!--        <div style="width: 500px; height: 500px; border: 3px solid red">-->
-<!--            <canvas id="line_chart" width="400" height="400"></canvas>-->
-<!--        </div>-->
-<!--    </div>-->
-    <div class="doughnut-canvas-container">
-        <canvas id="doughnut_chart"></canvas>
-<!--        <canvas class="chart" class="chartjs-render-monitor"></canvas>-->
+<div class="container">
+    <div class="chart_container">
+        <div>Pie Chart</div>
+        <div style="width: 500px; height: 500px; border: 3px solid red">
+            <canvas id="pie_chart" width="400" height="400"></canvas>
+        </div>
     </div>
-<!--    <div class="chart_container">-->
-<!--        <div>Pie Chart</div>-->
-<!--        <div style="width: 500px; height: 500px; border: 3px solid red">-->
-<!--            <canvas id="pie_chart" width="400" height="400"></canvas>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="chart_container">-->
-<!--        <div>Bar Chart</div>-->
-<!--        <div style="width: 500px; height: 500px; border: 3px solid red">-->
-<!--            <canvas id="bar_chart" width="400" height="400"></canvas>-->
-<!--        </div>-->
-<!--    </div>-->
 </div>
-<!--<script src="assets/line_chart.js"></script>-->
-<!--<script src="assets/doughnut_chart.js"></script>-->
 <!--<script src="assets/pie_chart.js"></script>-->
 <script>
 
-    const COLORS = [
-  "#2ecc71",
-  "#3498db",
-  "#95a5a6",
-  "#9b59b6",
-  "#f1c40f",
-  "#e74c3c",
-  "#34495e"
-];
+var pie_ctx = document.getElementById('pie_chart');
 
-var doughnut_ctx = document.getElementById('doughnut_chart');
-
-new Chart(doughnut_ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Green", "Blue", "Gray", "Purple", "Yellow", "Red", "Black"],
-    datasets: [{
-      backgroundColor: COLORS,
-      data: [12.25, 19, 3, 17, 28, 24, 7]
-    }]
-  },
-  options: {
-    layout: {
-        padding: 50
+var pie_chart = new Chart(pie_ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
-    legend: {
-      display: false
-    },
-    plugins: {
-      outlabels: {
-        backgroundColor: null,
-        color: COLORS,
-        //stretch: 30,
-        font: {
-          resizable: true,
-          minSize: 15,
-          maxSize: 20,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            },
+            x: {
+                ticks : {
+                    mirror: true
+                }
+            }
         },
-        zoomOutPercentage: 50,
-        //textAlign: 'start',
-      }
+        plugins: {
+              outlabels: false
+        },
+        tooltips: {
+            yAlign: 'bottom',
+            callbacks: {
+                labelColor: function(tooltipItem, chart) {
+                    return {
+                        backgroundColor: 'pink'
+                    }
+                },
+            },
+            backgroundColor: '#fff',
+            bodyFontColor: '#000',
+            // displayColors: false,
+            // borderWidth: 1,
+            // borderColor: '#22B0AF',
+            // boxHeight: 160,
+            // boxWidth: 200
+        }
     }
-  }
 });
 
 </script>
