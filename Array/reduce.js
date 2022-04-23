7b7e6616a0932d91391adb655b93735000722a23
Iman <imanali.cse@gmail.com>
2022-04-23 21:40:19 +0600

    reduce

---
+++ Array/reduce.js
@@ -0,0 +1,8 @@
+console.warn("How reduce() works without an initial value");
+const array = [15, 16, 17, 18, 19];
+function reducer(previous, current, index, array) {
+  const returns = previous + current;
+  console.log(`previous: ${previous}, current: ${current}, index: ${index}, returns: ${returns}`);
+  return returns;
+}
+array.reduce(reducer);
