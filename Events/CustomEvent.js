9ebd2f375350f04f26bc97b3b5fc8317a8c4ef0a
Iman <imanali.cse@gmail.com>
2022-05-11 10:36:12 +0600

    event

--- Events/CustomEvent.js
+++ Events/CustomEvent.js
@@ -2,6 +2,8 @@
 *
  * Why using custom events?
  * The custom events allow you to decouple the code that you want to execute after another piece of code completes.
+ *
+ *
  */
 
 // https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events#event_bubbling
@@ -19,3 +21,23 @@ form.addEventListener('awesome', e => console.log(e.detail.text()));
 
 // As the user types, the textarea inside the form dispatches/triggers the event to fire, and uses itself as the starting point
 textarea.addEventListener('input', e => e.target.dispatchEvent(eventAwesome));
+
+/**
+ * Custom events can be created in two ways:
+ *
+ * Using the Event constructor
+ * Using the CustomEvent constructor
+ */
+
+ const myEvent = new Event('myEvent', {
+  bubbles: true,
+  cancelable: true,
+  composed: false
+})
+
+const myEvent1 = new CustomEvent("myEvent1", {
+  detail: {},
+  bubbles: true,
+  cancelable: true,
+  composed: false,
+});
---
+++ Events/Events.js
@@ -0,0 +1,14 @@
+/**
+ * The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed,
+ * without waiting for stylesheets, images, and subframes to finish loading.
+ */
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+/**
+ * The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+ */
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+});
