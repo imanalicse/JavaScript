03a95075e23dbdbad27ab40910ee432e35c39b32
Iman <imanali.cse@gmail.com>
2022-05-21 13:31:02 +0600

    match

---
+++ String/match.js
@@ -0,0 +1,10 @@
+/**
+The match() method retrieves the result of matching a string against a regular expression
+ */
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+console.log(found); // Array ["T", "I"]
+
+// continue
