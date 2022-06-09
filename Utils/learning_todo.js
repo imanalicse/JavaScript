c42dec48553e63526f40291407ebd149d9c1ed01
Iman <imanali.cse@gmail.com>
2022-06-09 16:14:53 +0600

    learning todo

---
+++ Utils/learning_todo.js
@@ -0,0 +1,4 @@
+/*
+Looping through array and removing items, without breaking for loop
+https://stackoverflow.com/questions/9882284/looping-through-array-and-removing-items-without-breaking-for-loop
+*/
