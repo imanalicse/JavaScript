6d165198b1caa6896d42885190cf75c89d139fc4
Iman <imanali.cse@gmail.com>
2022-06-12 15:33:37 +0600

    regex test

--- RegExp/test.js
+++ RegExp/test.js
@@ -6,5 +6,34 @@ let regex = /org\/dashboard/;
 let url = 'https://olive.doyour.events/b/org/dashboard';
 console.log(regex.test(url));
 
-const regex2 = new RegExp('org\/dashboard');
-console.log(regex2.test(url));
+function isEventPage() {
+    let current_url = 'https://www.diywebsite.net.au/b/org/events/attendeeDetails/aac86c73-3cf6-4f77-94fa-6b72900f94d5';
+    let event_pages_regex_arr = [
+        /events\/overview/,
+        /events\/event-attendees/,
+        /events\/update/,
+        /events\/attendeeDetails/,
+        /surveys\/survey-details/,
+        /events\/event-lobby/,
+        /events\/breakout-room/,
+        /events\/event-invitees/,
+        /events\/campaign-list/
+    ]
+    let is_event_page = false;
+    for (let i = 0; i < event_pages_regex_arr.length; i++) {
+        let regex = event_pages_regex_arr[i];
+        console.log(regex)
+        if (regex.test(current_url)) {
+            is_event_page = true;
+            break;
+        }
+    }
+    return is_event_page;
+}
+
+
+console.log('is_event_page', isEventPage())
+
+
+// const regex2 = new RegExp('org\/dashboard');
+// console.log(regex2.test(url));
