c27a6bb2b79429ad6cc4836bf8fbfd80de4021a0
Iman <imanali.cse@gmail.com>
2022-06-18 17:36:16 +0600

    property accessor

---
+++ ExpressionAndOperator/PropertyAccessors.js
@@ -0,0 +1,17 @@
+/**
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_Accessors.
+* Property accessors provide access to an object's properties by using the dot notation or the bracket notation.
+
+Syntax:
+object.property
+object['property']
+*/
+
+const person1 = {};
+person1['firstname'] = 'Mario';
+person1['lastname'] = 'Rossi';
+console.log(person1.firstname); // expected output: "Mario"
+console.log(person1['firstname']); // expected output: "Mario";
+
+console.log(document.createElement('pre'))
+
