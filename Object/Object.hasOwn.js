89da178d7f37db9e073b78564f9cedfa10ef36f9
Iman <imanali.cse@gmail.com>
2022-07-20 08:39:56 +0600

    has own

---
+++ Object/Object.hasOwn.js
@@ -0,0 +1,7 @@
+/**
+ The Object.hasOwn() static method returns true if the specified object has the indicated property as its own property.
+ If the property is inherited, or does not exist, the method returns false.
+ Syntax:
+ Object.entries(obj)
+ */
+ //TODO
