94d5ad934a1ff9d8420104f8ac8418f79118eb65
Iman <imanali.cse@gmail.com>
2022-08-02 09:59:53 +0600

    for in loop

---
+++ StatementsAndDeclarations/for...in.js
@@ -0,0 +1,14 @@
+/**
+ * The for...in statement iterates over all enumerable properties of an object that are
+ * keyed by strings (ignoring ones keyed by Symbols), including inherited enumerable properties.
+ Syntax:
+ for (const variable in object) {
+   statement
+ }
+*/
+
+const object = { a: 1, b: 2, c: 3};
+
+for (const property in object) {
+  console.log(`${property}: ${object[property]}`);
+}
