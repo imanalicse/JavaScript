f7c6916a581890cf6c90cfb994f053a3c9e22de4
iman <iman@webalive.com.au>
2025-08-08 23:23:34 +1000

    Sortable

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
+import Sortable from 'sortablejs/modular/sortable.esm.js';
 console.log("Vite is running!");
 import './style.css';
+import './drag/drag-style.css'
+
+const el = document.getElementById('sortable-list');
+Sortable.create(el, {
+    handle: '.handle',
+    animation: 150,
+    onEnd: () => {
+        const order = Array.from(el.children).map(li => li.dataset.id);
+        console.log(order);
+    }
+});
